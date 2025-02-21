354d5693bb662744835ae11c32e2ef36b3507c93
Plexi09 <pierre.emmanuel.gobillard@gmail.com>
2025-02-21 12:38:18 +0100

    init

---
+++ src/main/java/me/plexi09/gravityChanger/GravityChanger.java
@@ -0,0 +1,17 @@
+package me.plexi09.gravityChanger;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class GravityChanger extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
